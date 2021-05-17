93d8417e7540aa375df319089f87103cc89f825d
Gina Barboni <76008560+Ginab972@users.noreply.github.com>
2021-05-17 11:05:47 -0400

    Update Story.js

--- components/Story.js
+++ components/Story.js
@@ -1,5 +1,4 @@
 export default function Story(story) {
-    console.log(story);
     return `
     <div class="story">
     <div> 
@@ -16,14 +15,12 @@ export default function Story(story) {
             ${story.comments_count} comments
           </a>
           |
-          <span class="favorite">
-            
-            Add To Favorites
+          <span class="favorite" data-story='${JSON.stringify(story)}'>
+          <img class="heart" src="images/heart.png"></img>
+          ${story.isFavorite ? "Remove From Favorites" : "Add To Favorites"}
           </span>
         </div>
       </div>
     </div>
   `;
 }
-
-// // <img class="heart" src="https://icon.now.sh/heart/ccc">
