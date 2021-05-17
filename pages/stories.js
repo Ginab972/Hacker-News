3e48a606fe68983542fa706ea9ecc351d6cc7d4a
Gina Barboni <76008560+Ginab972@users.noreply.github.com>
2021-05-17 11:08:32 -0400

    Update stories.js

--- pages/stories.js
+++ pages/stories.js
@@ -1,14 +1,26 @@
 import Story from '../components/Story.js';
 import view from '../utils/view.js';
 import baseUrl from '../utils/baseUrl.js';
+import checkFavorite from '../utils/checkFavorite.js';
+import store from '../store.js';
 
 export default async function Stories(path) {
+    const { favorites } = store.getState();
     const stories = await getStories(path);
     const hasStories = stories.length > 0;
 
     view.innerHTML = `<div>
-    ${hasStories ? stories.map((story, i) => Story({...story, index: i +1 })).join('') : 'No stories'}
-    </div>`;
+    ${hasStories ? stories.map((story, i) => Story({ ...story, index: i + 1, isFavorite: checkFavorite(favorites, story) })).join('') : 'No stories'}
+  </div>`;
+
+    document.querySelectorAll('.favorite').forEach(favoriteButton => {
+        favoriteButton.addEventListener('click', async function() {
+            const story = JSON.parse(this.dataset.story);
+            const isFavorited = checkFavorite(favorites, story);
+            store.dispatch({ type: isFavorited ? "REMOVE_FAVORITE" : "ADD_FAVORITE", payload: { favorite: story } })
+            await Stories(path);
+        });
+    });
 }
 
 async function getStories(path) {
@@ -23,18 +35,3 @@ async function getStories(path) {
     const stories = await response.json();
     return stories;
 }
-
-
-// https://node-hnapi.herokuapp.com
-
-// / (Top) -> /new
-
-//  / new (new)  https://news.ycombinator.com/newest
-
-//  / (ask) - >  https://news.ycombinator.com/ask
-
-//  / (show) -> https://news.ycombinator.com/show
-
-//  / (job) -> https://news.ycombinator.com/jobs
-
-//id, title, points, user, time, time_ago, comments_count, type, url, domain,
