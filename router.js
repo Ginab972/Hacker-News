3f2d85d0d18e6b9506b8ff8313b9d7cdc34083c2
Gina Barboni <76008560+Ginab972@users.noreply.github.com>
2021-05-17 08:50:24 -0400

    Add files via upload

---
+++ router.js
@@ -0,0 +1,26 @@
+import Stories from './pages/stories.js';
+import Item from './pages/item.js';
+
+const router = new Navigo(null, true, '#');
+
+export default class RouterHandler {
+    constructor() {
+        this.createRoutes()
+    }
+
+    createRoutes() {
+        const routes = [
+            { path: '/', page: Stories },
+            { path: '/new', page: Stories },
+            { path: '/ask', page: Stories },
+            { path: '/show', page: Stories },
+            { path: '/item', page: Item }
+        ];
+
+        routes.forEach(({ path, page }) => {
+            router.on(path, () => {
+                page(path);
+            }).resolve();
+        })
+    }
+}
